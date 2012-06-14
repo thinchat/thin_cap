bf1d4665aae4ef20bbdb4bcc54ebf74d090d21c6
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-14 19:27:44 -0400

    thin_core

--- thin_core_deploy.rb
+++ thin_core_deploy.rb
@@ -116,7 +116,7 @@ namespace :deploy do
     sudo "update-rc.d god-service defaults"
     sudo "service god-service start"
   end
-  after "deploy:setup_config", "deploy:god"
+  after "deploy", "deploy:god"
 
   desc "Create the database"
   task :create_database, roles: :app do
@@ -143,7 +143,7 @@ namespace :deploy do
 
   desc "Load environment-specific god configuration"
   task :god_config, roles: :app do
-    run "load #{release_path}/config/god/thin_core.#{rails_env}.god"
+    # sudo "god load #{release_path}/config/god/thin_core.#{rails_env}.god"
   end
   after "deploy:god", "deploy:god_config"
 
